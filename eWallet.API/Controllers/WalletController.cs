a31c7f9fa395a75d246011b3f6b066652adee8fb
Israel Iyonsi <israel.iyonsi@decagon.dev>
2021-11-27 23:09:11 +0100

    Feature: WalletController

---
+++ eWallet.API/Controllers/WalletController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eWallet.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalletController : ControllerBase
+    {
+        [HttpPost("Create")]
+        public IActionResult CreateWallet()
+        {
+            return Ok();
+        }
+
+        [HttpPut("Update")]
+        public IActionResult UpdateWallet()
+        {
+            return Ok();
+        }
+
+        [HttpGet("GetWallets/{Id}")]
+        public IActionResult GetAWalletById()
+        {
+            return Ok();
+        }
+
+        [HttpGet("GetAllWallets")]
+        public IActionResult GetAllWalletsById()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("Delete/{Id}")]
+        public IActionResult DeleteWallet()
+        {
+            return Ok(0);
+        }
+    }
+}
