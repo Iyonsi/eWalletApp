b215ede415522d8f8c8f67cd75ac6e17f20eb689
Israel Iyonsi <israel.iyonsi@decagon.dev>
2021-11-27 17:14:53 +0100

    Feature: Added UserController

---
+++ eWallet.API/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eWallet.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("UserDetails/{Id}")]
+        public IActionResult GetUsersById()
+        {
+            return Ok();
+        }
+       
+        [HttpGet("AllUsers")]
+        public IActionResult AllUsers()
+        {
+            return Ok();
+        }
+
+         [HttpGet("Role/{Id}")]
+         public IActionResult GetUsersByRole()
+         {
+             return Ok();
+         }
+
+
+         [HttpPut("ChangeRole")]
+         public IActionResult ChangeRole()
+         {
+             return Ok();
+         }
+
+         [HttpPut("Deactivate/{Id}/{UserId}")]
+         public IActionResult DeactivateUser()
+         {
+             return Ok();
+         }
+
+    }
+}
